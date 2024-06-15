a691ddf3e2c58824bdc3f53aae522700891e8f8e
RICCOM <ericomun@gmail.comgit>
2024-06-15 23:59:07 +0300

    nodes in tree

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,25 @@
 class Tree:
-  def __init__(self, root = None):
-    self.root = root
+    def __init__(self, root=None):
+        self.root = root
 
-  def get_element_by_id(self, id):
-    pass
+    def get_element_by_id(self, id):
+        if self.root is None:
+            return None
+
+        # List to store the nodes to visit
+        nodes_to_visit = [self.root]
+
+        # Traversing the tree
+        while nodes_to_visit:
+            # Get the next node to visit
+            node = nodes_to_visit.pop(0)
+
+            # Checking if the node's ID matches the desired ID
+            if node['id'] == id:
+                return node
+
+            # Adding the node's children to the list of nodes to visit
+            nodes_to_visit.extend(node['children'])
+
+        # Returning None if the desired node isn't found
+        return None
